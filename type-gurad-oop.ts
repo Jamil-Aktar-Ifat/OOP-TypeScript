0f51f715282f7db79dc3a6016edd492a26e3f7e3
ifat <i>
2024-11-08 21:28:16 +0000

    type guard using instance of

---
+++ type-gurad-oop.ts
@@ -0,0 +1,61 @@
+{
+  // instanceof guard
+  class Animal {
+    name: string;
+    species: string;
+
+    constructor(name: string, species: string) {
+      this.name = name;
+      this.species = species;
+    }
+    makeSound() {
+      console.log("bla bla bla");
+    }
+  }
+
+  class Dog extends Animal {
+    constructor(name: string, species: string) {
+      super(name, species);
+    }
+
+    makeBark() {
+      console.log("ghew ghew ghew");
+    }
+  }
+
+  class Cat extends Animal {
+    constructor(name: string, species: string) {
+      super(name, species);
+    }
+
+    makeMeaw() {
+      console.log("meaw meaw meaw");
+    }
+  }
+
+  // using function to handle the function or narrowing the function
+  const isDog = (animal: Animal): animal is Dog => {
+    return animal instanceof Dog;
+  };
+
+  const isCat = (animal: Animal): animal is Cat => {
+    return animal instanceof Cat;
+  };
+
+  const getAnimal = (animal: Animal) => {
+    if (isDog(animal)) {
+      animal.makeBark();
+    } else if (isCat(animal)) {
+      animal.makeMeaw();
+    } else {
+      animal.makeSound();
+    }
+  };
+
+  const dog = new Dog("Dog bro", "dog");
+  const cat = new Cat("Cat bro", "cat");
+
+  getAnimal(cat);
+
+  //
+}
